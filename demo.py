9688b9838ddbbdbd4ff851be7400853179ddc8e9
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2020-03-11 21:26:37 +1030

    demo

---
+++ demo.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+
+
+
+
+
+
+arr1 = np.arange(10)
+arr2 = np.arange(10)
+val = arr1 + arr2
+
+
+
+
+
+val = np.array([[1, 2, 3],[4, 5, 6]])
+print(val.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
